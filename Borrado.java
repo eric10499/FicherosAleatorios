9bd6d7f73670307d41eb11e6af9ee1d99bc553e0
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-12-15 18:35:11 +0100

    Update Borrado.java

--- Borrado.java
+++ Borrado.java
@@ -1,3 +1,29 @@
+/*
+ * Borrado.java
+ * 
+ * Copyright 2018 ERIC <ERIC@DESKTOP-5T1N085>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * BORRADO. Crea un programa Java que al ejecutarlo desde la línea de comandos reciba un identificador de empleado y lo borre. Se hará un borrado lógico marcando el registro con la siguiente información: el identificador será igual a -1, el apellido será igual al identificador que se borra, y el departamento y salario serán 0.
+ * 
+ * Fet per Eric Visier Sánchez
+ * 
+ */
+
 package FicherosAleatorios;
 
 import java.io.EOFException;
