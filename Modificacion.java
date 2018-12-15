114bf12d8aef7f57cc05daaa7e88b61f3c300162
eric10499 <34514368+eric10499@users.noreply.github.com>
2018-12-15 18:39:27 +0100

    Update Modificacion.java

--- Modificacion.java
+++ Modificacion.java
@@ -1,3 +1,29 @@
+/*
+ * Modificacion.java
+ * 
+ * Copyright 2018 ERIC <ERIC@DESKTOP-5T1N085>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * MODIFICACION.  Crea un programa Java que reciba desde la línea de comandos un identificador de empleado y un importe. Se debe realizar la modificación del salario. La modificación consistirá en sumar al salario del empleado el importe introducido. El programa debe visualizar el apellido, el salario antiguo y el nuevo. Si el identificador no existe se visualizará mensaje indicándolo.
+ * 
+ * Fet per Eric Visier Sánchez
+ * 
+ */
+
 package FicherosAleatorios;
 
 import java.io.EOFException;
